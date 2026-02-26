af30d8fc1197c69961b8d0c18c6e82e242012da1
Lucas Avelar <lucasavelardesign@gmail.com>
2026-02-26 09:50:17 -0300

    Atualiza o endpoint api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,39 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECt 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseNameResult = await database.query("SHOW event_source;");
+  const databaseNameValue = databaseNameResult.rows[0].event_source;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    // Limpa a string para evitar SQL Injection, mesmo que o valor venha de uma variável de ambiente
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        name: databaseNameValue,
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,25 @@
 test("GET /api/v1/status retorna status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  // Verifica se o status da resposta é 200
   expect(response.status).toBe(200);
+
+  // Verifica se o campo updated_at existe e é uma data válida
+  expect(responseBody).toHaveProperty("updated_at");
+  const date = new Date(responseBody.updated_at).toISOString();
+  const updatedAt = responseBody.updated_at;
+  expect(date).toEqual(updatedAt);
+
+  // Verifica o nome da base de dados
+  expect(responseBody.dependencies.database.name).toBe("PostgreSQL");
+
+  // Versão da base de dados
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  // Conexões máximas
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  // Conexões ativas
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
