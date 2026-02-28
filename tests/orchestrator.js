6377f92a809629128a6888a04b3e9ea8cfe8beca
Lucas Avelar <lucasavelardesign@gmail.com>
2026-02-28 19:32:52 -0300

    Refatora os testes deixando mais otimizados

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,6 +6,8 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      minTimeout: 200,
+      maxTimeout: 500,
     });
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
